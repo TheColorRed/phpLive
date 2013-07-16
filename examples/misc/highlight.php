750ae8ec2b064776a978f0660247f02fd8684286
RyanNaddy <RyanNaddy@g3m242.Group3.local>
2013-07-16 11:47:24 -0500

    Update Highlight examples

--- examples/misc/highlight.php
+++ examples/misc/highlight.php
@@ -1,13 +1,16 @@
 <?php
+
 require_once "../../phpLive.php";
 echo "<h3>Html File</h3>";
-echo "<pre>".$live->highlight("test_html.html", HIGHLIGHT_HTML, INPUT_FILE)->tabToSp()."</pre>";
+echo "<pre>" . $live->highlight("highlights/test_html.html", HIGHLIGHT_HTML)->tabToSp() . "</pre>";
 echo "<hr />";
-echo "<h3>Xml File</h3>";
-echo "<pre>".$live->highlight("test_xml.xml", HIGHLIGHT_HTML, INPUT_FILE)->tabToSp()."</pre>";
-echo "<hr />";
-echo "<h3>CSS File</h3>";
-echo "<pre>".$live->highlight("test_css.css", HIGHLIGHT_CSS, INPUT_FILE)->tabToSp()."</pre>";
 
+echo "<h3>Xml File</h3>";
+echo "<pre>" . $live->highlight("highlights/test_xml.xml", HIGHLIGHT_HTML)->tabToSp() . "</pre>";
 echo "<hr />";
-echo $live->highlight("highlight.php", HIGHLIGHT_PHP, INPUT_FILE);
+
+echo "<h3>CSS File</h3>";
+echo "<pre>" . $live->highlight("highlights/test_css.css", HIGHLIGHT_CSS)->tabToSp() . "</pre>";
+echo "<hr />";
+
+echo $live->highlight("highlight.php", HIGHLIGHT_PHP);
